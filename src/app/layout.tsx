672dbaeabf64d8a72dacf80b5c08738e0295c4fe
masonliiu <liumasn@gmail.com>
2026-01-02 19:04:18 -0600

    Set dark defaults to avoid refresh flash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="mocha" suppressHydrationWarning>
       <head>
         <Script
           id="theme-init"
